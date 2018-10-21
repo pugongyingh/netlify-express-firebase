c3cd36018e0f8df93222a89c4008730c6b5c7e12
Stanislau Shliakhtsich <stasgm@gmail.com>
2018-10-21 22:06:58 +0300

    wip

--- express/server.js
+++ express/server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+var firebase = require("firebase");
+// 
+var config = require("./firebase");
+var scrapeSelectBy = require("./rateScape");
+firebase.initializeApp(config);
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -13,8 +18,26 @@ router.get('/', (req, res) => {
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
+router.get('/rates', (req, res) => {
+  scrapeSelectBy().then(list => {
+    console.log("done!");
+    
+    saveToDatabase(list);     
+  });
+})
+
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+
+function saveToDatabase(data) {
+  console.log('saving..');
+  //Insert key,value pair to database
+  firebase
+    .database()
+    .ref("/CurrencyRate")
+    .set(data);
+}
 
 module.exports = app;
 module.exports.handler = serverless(app);
